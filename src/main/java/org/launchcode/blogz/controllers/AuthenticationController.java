6b2335178ae005030481a4d1965c9a10a2386985
TJPoth <tjpothier17@gmail.com>
2016-12-11 14:30:30 -0600

    Woo! Added dynamic titles and h1 tags! 12/11/2016; 2:30 PM

--- src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
@@ -1,35 +1,40 @@
 package org.launchcode.blogz.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthenticationController {
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("pageTitle", "Home");
 		return "base";
 	}
 	
 	@RequestMapping(value="/signup", method=RequestMethod.GET)
-	public String signupForm() {
+	public String signupForm(Model model) {
+		model.addAttribute("pageTitle", "Sign up");
 		return "signup";
 	}
 	
 	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public String loginForm() {
+	public String loginForm(Model model) {
+		model.addAttribute("pageTitle", "Log in");
 		return "login";
 	}
 	
 	@RequestMapping(value="/newpost", method=RequestMethod.GET)
-	public String newpostForm() {
+	public String newpostForm(Model model) {
+		model.addAttribute("pageTitle", "New Post");
 		return "newpost";
 	}
 	
 	@RequestMapping(value="/blog", method=RequestMethod.GET)
-	public String blogForm() {
+	public String blogForm(Model model) {
+		model.addAttribute("pageTitle", "All Posts");
 		return "blog2";
 	}
 
